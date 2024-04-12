2fec2ba9b902bc91b113606db67e7880c2b68bfe
shinobywolf <105944752+shinobywolf@users.noreply.github.com>
2024-04-12 11:38:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T90:40:19");
+const tempoObjetivo2 = new Date("2024-12-05T90:50:10");
+const tempoObjetivo3 = new Date("2024-12-30T60:00:10");
+const tempoObjetivo4 = new Date("2024-08-01T30:50:10");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
